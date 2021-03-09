2ed4d8f08b0f9fab60fbe43e9770b7114438089d
Aashrith Kandimalla <aashrith.kandimalla@corp.knowledgelens.com>
2021-03-09 14:20:58 +0530

    first commit

---
+++ map.js
@@ -0,0 +1,27 @@
+const people = [
+    {
+        name : 'A',
+        age: 21,
+        work: 'aa',
+    },
+    {
+        name : 'B',
+        age: 22,
+        work: 'ab',
+    },
+    {
+        name : 'C',
+        age: 23,
+        work: 'ac',
+    },
+];
+
+const getAges = (person) => person.age;
+
+const ages = people.map((getAges) => {
+    console.log(person.age)
+})
+
+
+
+console.log(ages)
